3cc8171396455e62f9ac0c3d9b95f2780547b613
panteleimonov <panteleimonov@yahoo.com>
2019-12-15 23:47:36 +0200

    version 3

--- src/components/list-item/list-item.jsx
+++ src/components/list-item/list-item.jsx
@@ -1,25 +1,50 @@
 import React from 'react';
 import './list-item.css';
 
-const ListItem = ({ label, important = false }) => {
-    const itemStyle = {
-        color: important ? 'tomato' : 'inherit',
-        'letterSpacing': '1px',
-    };
+export default class ListItem extends React.Component {
 
-    return (
-        <span className="list-item">
-            <span style={itemStyle} className="list-item-text">
-                {label}
+    render() {
+        const { label, important = false } = this.props;
+        const itemStyle = {
+            color: important ? 'tomato' : 'inherit',
+            'letterSpacing': '1px',
+        };
+    
+        return (
+            <span className="list-item">
+                <span style={itemStyle} className="list-item-text">
+                    {label}
+                </span>
+                <button type="button" className="btn btn-outline-success btn-sm">
+                    <i className="fa fa-exclamation" />
+                </button>
+                <button type="button" className="btn btn-outline-success btn-sm style-red">
+                    <i className="fa fa-trash-o" />
+                </button>
             </span>
-            <button type="button" className="btn btn-outline-success btn-sm">
-                <i className="fa fa-exclamation" />
-            </button>
-            <button type="button" className="btn btn-outline-success btn-sm style-red">
-                <i className="fa fa-trash-o" />
-            </button>
-        </span>
-    )
-}
+        )
+    }
 
-export default ListItem;
+} // class ListItem
+
+
+// const ListItem_func = ({ label, important = false }) => {
+//     const itemStyle = {
+//         color: important ? 'tomato' : 'inherit',
+//         'letterSpacing': '1px',
+//     };
+
+//     return (
+//         <span className="list-item">
+//             <span style={itemStyle} className="list-item-text">
+//                 {label}
+//             </span>
+//             <button type="button" className="btn btn-outline-success btn-sm">
+//                 <i className="fa fa-exclamation" />
+//             </button>
+//             <button type="button" className="btn btn-outline-success btn-sm style-red">
+//                 <i className="fa fa-trash-o" />
+//             </button>
+//         </span>
+//     )
+// }
